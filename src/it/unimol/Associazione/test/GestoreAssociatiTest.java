97a3cfe4bffd01dd2602051d98757ae30845c59e
Andrea <atticoandrea@gmail.com>
2025-08-12 18:04:55 +0200

    Implementazione casi di test

---
+++ src/it/unimol/Associazione/test/GestoreAssociatiTest.java
@@ -0,0 +1,37 @@
+package it.unimol.Associazione.test;
+
+import it.unimol.Associazione.app.Associato;
+import it.unimol.Associazione.app.GestoreAssociati;
+import it.unimol.Associazione.exceptions.InvalidAssociatoException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GestoreAssociatiTest {
+
+    //bisogna eseguire prima add test e poi remove test
+    @Test
+    public void addTest()throws InvalidAssociatoException {
+        GestoreAssociati g = GestoreAssociati.getInstance();
+        Associato a = new Associato("Test Nome", "Test Cognome", "Test email",
+                "test telefono", 0 );
+        g.addSocio(a);
+        Associato a2 = g.getSocio(0);
+        assertEquals("Test Nome", a2.getNome());
+        assertEquals("Test Cognome", a2.getCognome());
+        assertEquals("Test email", a2.getEmail());
+        assertEquals("test telefono", a2.getTelefono());
+    }
+
+    @Test
+    public void removeTest() throws InvalidAssociatoException {
+        GestoreAssociati g = GestoreAssociati.getInstance();
+        Associato a = new Associato("Test Nome", "Test Cognome", "Test email",
+                "test telefono", 0 );
+        g.removeSocio(a);
+        assertThrows(InvalidAssociatoException.class, () -> {
+            g.getSocio(0);
+        });
+    }
+
+}
+
